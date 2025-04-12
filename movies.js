16ea12591a107e403605cee47b6b88fb4e6a20b8
Uliana Ksiondz <166907543+lianaksndz@users.noreply.github.com>
2025-04-12 10:09:18 +0300

    Add movies.js file with movie that are showing

---
+++ movies.js
@@ -0,0 +1,20 @@
+const movies = [
+  {
+    id: "1",
+    title: "Інтерстеллар",
+    description: "Науково-фантастичний фільм про подорож у космосі.",
+    genre: "Sci-Fi",
+    datetime: "2025-04-15 18:00",
+    poster: "https://i.imgur.com/e1hLQ2F.jpeg",
+  },
+  {
+    id: "2",
+    title: "Темний лицар",
+    description: "Бетмен проти Джокера.",
+    genre: "Action",
+    datetime: "2025-04-15 20:00",
+    poster: "https://i.imgur.com/3fJ1P48.jpeg",
+  },
+];
+
+export default movies;
